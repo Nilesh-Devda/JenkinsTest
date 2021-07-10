de2a6993ddc9f13491d1d637533a0ba00e169f63
Nilesh Devda <nileshdevra47@gmail.com>
2021-07-10 18:09:53 +0530

    File Modified

--- PythonCodeTest.py
+++ PythonCodeTest.py
@@ -4,5 +4,9 @@ def Name(MyName):
 def Live(City):
     return "I live in " + City
 
+def Eat(Food):
+    return "I like to eat" + Food
+
 print(Name("Nilesh Devda"))
 print(Live("Pune")) 
+print(Eat("Burger"))
